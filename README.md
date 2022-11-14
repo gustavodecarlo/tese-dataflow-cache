4c3ed9c4b670db3ce3f30483f21c221fc3c12450
Gustavo Decarlo <gustavodecarlotw@Gustavos-MacBook-Pro.local>
2022-11-14 18:33:11 -0300

    chore: partial solution of containment problem

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 
 ## Criação do cluster k8s com o kind para o cenário da tese
 
-    $ kind cluster create
+    $ kind create cluster
 
 ## Airflow, spark-operator e as dags para as simulações
 
@@ -32,6 +32,9 @@
     - Com dag de exemplo de como utilizar o operador do airflow com o spark operator.
     - Para o cenário da tese: GCS com o delta lake (fragmentos) e Cassandra (Operaçoes) 
     - Desenvolver a biblioteca que vai salvar o fragmento de dados e as operações do dataflow em pyspark
+
+
+gcloud beta container --project "gdecarlo-cloud-tests" clusters create "cluster-tese-foresee" --zone "us-central1-c" --no-enable-basic-auth --cluster-version "1.22.11-gke.400" --release-channel "regular" --machine-type "e2-standard-2" --image-type "COS_CONTAINERD" --disk-type "pd-standard" --disk-size "40" --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --max-pods-per-node "110" --num-nodes "3" --logging=SYSTEM,WORKLOAD --monitoring=SYSTEM --enable-ip-alias --network "projects/gdecarlo-cloud-tests/global/networks/default" --subnetwork "projects/gdecarlo-cloud-tests/regions/us-central1/subnetworks/default" --no-enable-intra-node-visibility --default-max-pods-per-node "110" --no-enable-master-authorized-networks --addons HorizontalPodAutoscaling,HttpLoadBalancing,GcePersistentDiskCsiDriver --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0 --enable-shielded-nodes --node-locations "us-central1-c"
 
 ## Próximos passos
 
---
+++ datafrag-manager/datafrag_manager/utils.py
@@ -0,0 +1,93 @@
+import re
+import logging
+
+import boolean
+from sqloxide import parse_sql
+from pyspark.sql.session import SparkSession, DataFrame
+
+
+logger = logging.getLogger(__name__)
+
+def nested_dict_pairs_iterator(dict_obj):
+    ''' 
+        This function accepts a nested dictionary as argument
+        and iterate over all values of nested dictionaries
+    '''
+    
+    for key, value in dict_obj.items():
+        if isinstance(value, dict):
+            for pair in nested_dict_pairs_iterator(value):
+                yield (key, *pair)
+        else:
+            yield (key, value)
+
+
+def convert_filter_to_dnf(filter: str) -> str:
+    algebra = boolean.BooleanAlgebra()
+    m = re.finditer(r'([A-Za-z0-9\#]+)\s*(?:=|<=|>=|>|<)\s*(?:\d+(?:\.\d+)?)|(and|or|AND|OR)\s+|(\w+\([A-Za-z0-9\#]+\))', filter)
+    elements = {}
+    old_element = ''
+    idx = 0 
+    for o in m:
+        if '#' in o.group():
+            element = re.findall(r'[A-Za-z0-9\#]+', re.sub(r'\w+\(','',o.group()))[0].split('#')[0]
+            element_idx = f'{element}{idx}'
+            term = o.group()
+            if old_element == element:
+                term = f'{old_term} AND {o.group()}'
+            elements[element_idx] = f'({term})'
+            filter = filter.replace(o.group(), element_idx)
+            old_element = element
+            old_term = o.group()
+            idx += 1
+
+    expression = algebra.parse(filter, simplify=False)
+    exprdnf = algebra.dnf(expression)
+    strdnf = str(exprdnf)
+    logger.debug(f'Boolean expression: {expression}')
+    logger.debug(f'Boolean DNF expression: {exprdnf}')
+    for key, value in elements.items():
+        strdnf = strdnf.replace(key, value)
+    strdnf = strdnf.replace('&', ' AND ').replace('|', ' OR ')
+    return strdnf
+
+def get_metadata_table_representation(spark_session: SparkSession, dataflow: str, task: str, datasource: str, metadata: dict) -> DataFrame:
+    sql = f'''
+        SELECT *
+        FROM {dataflow}
+        WHERE {metadata.get("operation").get("filter").get("value_dnf")}
+    '''
+    output = parse_sql(sql=sql, dialect='spark')
+    where_condition = output[0].get('Query').get('body').get('Select').get('selection')
+    old_term = None
+    table_repr = [{}]
+    term = 1
+    for pair in nested_dict_pairs_iterator(where_condition):
+        if 'value' in pair[-2]:
+            if old_term and old_term != pair[-1]:
+                table_repr.append({})
+            table_repr[len(table_repr)-1].update({'dataflow': dataflow})
+            table_repr[len(table_repr)-1].update({'task': task})
+            table_repr[len(table_repr)-1].update({'datasource': datasource})
+            table_repr[len(table_repr)-1].update({'attribute': pair[-1].split('#')[0]})
+            old_term = pair[-1]
+        if 'op' in pair[-2]:
+            # tratar o novo termo e adicionando a lista esse novo elemento
+            if 'Or' in pair[-1]:
+                table_repr.append({})
+                term = term + 1
+                old_term = None
+            
+            table_repr[len(table_repr)-1].update({'term': term})
+            operation = pair[-1]
+        if 'Number' in pair[-2]:
+            if 'GtEq' in operation or 'Gt' in operation:
+                table_repr[len(table_repr)-1].update({'min': float(pair[-1][0])})
+            if operation == 'Eq':
+                table_repr[len(table_repr)-1].update({'min': float(pair[-1][0])})
+                table_repr[len(table_repr)-1].update({'max': float(pair[-1][0])})
+            if 'LtEq' in operation or 'Lt' in operation:
+                table_repr[len(table_repr)-1].update({'max': float(pair[-1][0])})
+            
+    logger.debug(table_repr)
+    return spark_session.createDataFrame(data=table_repr)
